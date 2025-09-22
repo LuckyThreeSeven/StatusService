3c0fcfb0a7547716b0d746e2380c0d320cbc18da
himinqq <alsrud0966@naver.com>
2025-09-22 14:19:12 +0900

    feat: 블랙박스 간단한 3티어로 구현

---
+++ status/src/main/java/com/neves/status/repository/Blackbox.java
@@ -0,0 +1,24 @@
+package com.neves.status.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter; // Add this
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter // Add this
+@Entity
+@Builder // Add this
+public class Blackbox {
+	@Id
+	private String uuid;
+	private String nickname;
+	private String userId;
+	private LocalDateTime createdAt;
+}
