e3efd5524268dda820f0e2a27d6f3b77fb3afbf9
donggyu <leedong1848@gmail.com>
2025-09-22 11:27:46 +0900

    feat: 메타 데이터 간단한 3티어로 구현

---
+++ status/src/main/java/com/neves/status/service/MetadataService.java
@@ -0,0 +1,47 @@
+package com.neves.status.service;
+
+import com.neves.status.controller.dto.blackbox.MetadataRegisterRequest;
+import com.neves.status.controller.dto.metadata.MetadataResponse;
+import com.neves.status.repository.Metadata;
+import com.neves.status.repository.MetadataRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MetadataService {
+	private final MetadataRepository repository;
+
+	public void create(UUID metadataId, MetadataRegisterRequest request) {
+		Metadata metadata = Metadata.builder()
+				.id(metadataId.toString())
+				.blackboxUuid(request.getBlackbox_uuid())
+				.streamStartedAt(request.getStream_started_at())
+				.createdAt(request.getCreated_at())
+				.fileSize(request.getFile_size())
+				.duration(request.getDuration())
+				.objectKey(request.getObject_key())
+				.fileType(request.getFile_type())
+				.build();
+		repository.save(metadata);
+	}
+
+	public List<MetadataResponse> list(String blackboxId, LocalDateTime date) {
+		List<Metadata> metadataList = repository.findMetadataByBlackboxUuid(blackboxId, date);
+		return metadataList.stream()
+				.map(data -> MetadataResponse.builder()
+						.object_key(data.getObjectKey())
+						.duration(data.getDuration())
+						.created_at(data.getCreatedAt())
+						.file_size(data.getFileSize())
+						.file_type(data.getFileType()).build()
+				).toList();
+	}
+
+	public void delete(String metadataId) {
+		repository.deleteById(metadataId);
+	}
+}
