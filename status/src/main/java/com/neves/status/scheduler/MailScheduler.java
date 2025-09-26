89f097b53ba1d652a3978d0a8d5cf6a29a155272
donggyu <leedong1848@gmail.com>
2025-09-26 10:45:21 +0900

    refactor: 로그 추가,매직넘저  상수로 변경

--- status/src/main/java/com/neves/status/scheduler/MailScheduler.java
+++ status/src/main/java/com/neves/status/scheduler/MailScheduler.java
@@ -3,6 +3,7 @@ package com.neves.status.scheduler;
 import com.neves.status.feign.MailClient;
 import com.neves.status.feign.MailDto;
 import com.neves.status.service.BlackboxService;
+import feign.FeignException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -28,12 +29,20 @@ public class MailScheduler {
 		for (BlackboxMailDto blackbox : disconnectedBlackbox) {
 			LocalDateTime dateLastConnected = sentBlackboxes.get(blackbox.getBlackboxId());
 			if (Objects.isNull(dateLastConnected) || !dateLastConnected.isEqual(blackbox.getLastConnectedAt())) {
-				mailClient.sendMail(MailDto.builder()
-						.to(blackbox.getUserId())
-						.format(MailClient.BLACKBOX_UNCONNECTED)
-						.parameters(List.of(blackbox.getNickname(), blackbox.getLastConnectedAt().toString()))
-						.build());
-				sentBlackboxes.put(blackbox.getBlackboxId(), blackbox.getLastConnectedAt());
+				log.info("Sending mail to {}, lastConnectedAt: {}", blackbox.getUserId(), blackbox.getLastConnectedAt().toString());
+				try {
+					mailClient.sendMail(MailDto.builder()
+							.to(blackbox.getUserId())
+							.format(MailClient.BLACKBOX_UNCONNECTED)
+							.parameters(List.of(blackbox.getNickname(),
+									blackbox.getLastConnectedAt().toString()))
+							.build());
+					sentBlackboxes.put(blackbox.getBlackboxId(), blackbox.getLastConnectedAt());
+				} catch (FeignException e) {
+					log.info("Fail to connect the mail server, blackboxId: {}, status: {}, response: {}", blackbox.getBlackboxId(), e.status(), e.contentUTF8());
+				} catch (RuntimeException e) {
+					log.info("A exception occurred while connect the mail server, blackboxId: {}", blackbox.getBlackboxId());
+				}
 			}
 		}
 	}
